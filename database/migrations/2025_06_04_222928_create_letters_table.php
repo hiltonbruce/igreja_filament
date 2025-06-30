705ed5d339ee8e6689e756a97d9e81497324965d
Joseilton Costa Bruce <hiltonbruce@gmail.com>
2025-06-30 13:58:27 -0300

    Atualizado p time zone

--- database/migrations/2025_06_04_222928_create_letters_table.php
+++ database/migrations/2025_06_04_222928_create_letters_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('letters', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->timestampsTz();
         });
     }
 
